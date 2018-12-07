6ebffee97ba6e1e73066e47a306a041aafea8434
Josh McCall <joshmccall221@gmail.com>
2018-12-06 21:17:15 -0800

    #!

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var exec = require('child_process').exec, child;
+#!/usr/bin/env node
 
-var myscript = exec("./corber.sh");
+var exec = require('child_process').exec, child;
+//console.log(process.cwd())
+console.log(process.mainModule.paths[1])
+var myscript = exec(process.mainModule.paths[1]+"/corber-react/corber.sh");
 myscript.stdout.on("data",function(data){
     console.log(data); // process output will be displayed here
 });
